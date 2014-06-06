a107dd0aa92fb5e718e45ca57e16939de034a911
Daniel <petrovaliev95@abv.bg>
2014-06-06 22:09:08 +0300

    repair bullshits

--- C# Basics март 2014/Lectures/6.Цикли/6. Loops-Demos/Continue/SumOdd.cs
+++ C# Basics март 2014/Lectures/6.Цикли/6. Loops-Demos/Continue/SumOdd.cs
@@ -1,6 +1,21 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio 2013
-VisualStudioVersion = 12.0.21005.1
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "01. Numbers from 1 to N", "01. Numbers from 1 to N\01. Numbers from 1 to N.csproj", "{B5B2D6AB-90AC-4E1A-9708-31AD8E80D137}"
+using System;
+
+class SumOdd
+{
+	static void Main()
+	{
+		Console.Write("n = ");
+        string numberAsString = Console.ReadLine();
+        int n = int.Parse(numberAsString);
+
+		int sum = 0;
+
+		for (int i = 1; i <= n; i += 2)
+		{
+			if ((i % 7) == 0)
+				continue;
+			sum += i;
+		}
+		Console.WriteLine("sum = {0}", sum);
+	}
+}
