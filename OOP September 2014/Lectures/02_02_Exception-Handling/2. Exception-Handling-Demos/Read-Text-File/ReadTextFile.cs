7875d51ca09b2d1b2cef19b3b93431f747189002
Daniel <petrovaliev95@abv.bg>
2014-09-24 11:11:31 +0300

    add lecture "Exception-Handling" in "OOP September 2014"

---
+++ OOP September 2014/Lectures/02_02_Exception-Handling/2. Exception-Handling-Demos/Read-Text-File/ReadTextFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class ReadTextFile
+{
+    static void Main()
+    {
+        StreamReader reader = new StreamReader(@"..\..\ReadTextFile.cs");
+        using (reader)
+        {
+            int lineNumber = 0;
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                lineNumber++;
+                Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
