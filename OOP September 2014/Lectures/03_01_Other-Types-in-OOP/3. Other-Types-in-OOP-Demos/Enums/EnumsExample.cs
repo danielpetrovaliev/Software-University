6a05e6541239efa1e1131cc7c9d4a2f613dcb62e
Daniel <petrovaliev95@abv.bg>
2014-09-24 18:08:23 +0300

    add lecture "Other-Types-in-OOP-Demos" in "OOP September 2014"

---
+++ OOP September 2014/Lectures/03_01_Other-Types-in-OOP/3. Other-Types-in-OOP-Demos/Enums/EnumsExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum DayOfWeek
+{
+	Mon, Tue, Wed, Thu, Fri, Sat, Sun
+}
+
+class EnumExample
+{
+	static void Main()
+	{
+		DayOfWeek day = DayOfWeek.Wed;
+		Console.WriteLine(day); // Wed
+		
+		Console.WriteLine((int)day); // 2
+
+		day = DayOfWeek.Sat;
+        Console.WriteLine(++day); // Sun
+        Console.WriteLine(++day); // 7
+
+		day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), "Mon");
+		Console.WriteLine(day); // Mon
+	}
+}
