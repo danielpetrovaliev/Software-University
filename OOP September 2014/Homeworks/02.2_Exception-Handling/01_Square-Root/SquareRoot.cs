13693f984d4dae83be4bb3ea65dbb1e378eda368
Daniel <petrovaliev95@abv.bg>
2014-09-25 23:10:17 +0300

    add homework "Exception Handling" in "OOP September 2014"

---
+++ OOP September 2014/Homeworks/02.2_Exception-Handling/01_Square-Root/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Square_Root
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number to calculate square root: ");
+                double number = Double.Parse(Console.ReadLine());
+                double square = Math.Sqrt(number);
+                Console.WriteLine("Square root of number is: {0}", square);
+
+            }
+            catch (FormatException)
+            {
+                
+                throw new FormatException("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
---
+++ OOP September 2014/Homeworks/02.2_Exception-Handling/02_EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Enter_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            for (int i = 0; i < 10; i++)
+            {
+                ReadNumber(start, end);
+            }
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            int num = 0;
+            Console.Write("Enter number in range[{0}...{1}]: ", start, end);
+            num = int.Parse(Console.ReadLine());
+            try
+            {
+                if (!(num <= end && num >= start))
+                {
+                    while (!(num <= end && num >= start))
+                    {
+                        Console.Write("You number is not in range. Please enter again: ");
+                        num = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Invalid Number");
+            }
+            return num;
+        }
+    }
+}
