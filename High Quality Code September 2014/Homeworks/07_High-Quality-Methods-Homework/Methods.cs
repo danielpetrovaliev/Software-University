fa2024907ae01f21292b709f014b1ecff4573644
Daniel <petrovaliev95@abv.bg>
2014-10-24 13:39:08 +0300

    add homework "High Quality Methods" in "High Quality Code September 2014"

---
+++ High Quality Code September 2014/Homeworks/07_High-Quality-Methods-Homework/Methods.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Sides should be positive.");
+            }
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        static string NumberToDigit(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    return "zero";
+                    break;
+                case 1:
+                    return "one";
+                    break;
+                case 2:
+                    return "two";
+                    break;
+                case 3:
+                    return "three";
+                    break;
+                case 4:
+                    return "four";
+                    break;
+                case 5:
+                    return "five";
+                    break;
+                case 6:
+                    return "six";
+                    break;
+                case 7:
+                    return "seven";
+                    break;
+                case 8:
+                    return "eight";
+                    break;
+                case 9:
+                    return "nine";
+                    break;
+                default:
+                    return "Invalid number!";
+                    break;
+            }
+        }
+
+        static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new ArgumentNullException("Elements cannot be null.");
+            }
+
+            int maxNumber = 0;
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i] > maxNumber)
+                {
+                    maxNumber = elements[i];
+                }
+            }
+            return maxNumber;
+        }
+
+        static void PrintAsNumber(object number, string format)
+        {
+            switch (format)
+            {
+                case "f":
+                    Console.WriteLine("{0:f2}", number);
+                    break;
+                case "%":
+                    Console.WriteLine("{0:p0}", number);
+                    break;
+                case "r":
+                    Console.WriteLine("{0,8}", number);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid format.");
+                    break;
+            }
+        }
+
+
+        static double CalcDistance(double x1, double y1, double x2, double y2, 
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = false;
+            isVertical = false;
+
+            if (y1.Equals(y2))
+            {
+                isHorizontal = true;
+            }
+            if (x1.Equals(x2))
+            {
+                isVertical = true;
+            }
+
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+            
+            Console.WriteLine(NumberToDigit(5));
+            
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+            
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
+            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+
+            Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
+            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
